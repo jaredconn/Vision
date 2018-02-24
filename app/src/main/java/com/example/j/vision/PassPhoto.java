88426c4e8ab1829d8b3b26a757f2657c70b76da9
jaredconn <jwc374@humboldt.edu>
2018-02-23 20:26:41 -0800

    contacts google api, gets a response back, recognizes image

--- app/src/main/java/com/example/j/vision/PassPhoto.java
+++ app/src/main/java/com/example/j/vision/PassPhoto.java
@@ -1,12 +1,15 @@
 package com.example.j.vision;
 
+//https://developer.android.com/things/training/doorbell/cloud-vision.html
 
+import android.app.Activity;
 import android.content.AsyncQueryHandler;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import android.graphics.Matrix;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
@@ -45,10 +48,10 @@ import java.util.Map;
 
 public class PassPhoto extends AppCompatActivity {
 
-     private static final String TAG = "PassPhoto.java";
+    private static final String TAG = "PassPhoto.java";
 
     private static final String CLOUD_VISION_API_KEY = "AIzaSyCPeO7TayOnyDMwaarp_HKV9g3guJN2qi4";
-   // private static final MAX_LABEL_RESULTS = 10;
+    // private static final MAX_LABEL_RESULTS = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,42 +59,47 @@ public class PassPhoto extends AppCompatActivity {
         setContentView(R.layout.activity_pass_photo);
 
 
+        final ImageView IMG;
+        IMG = (ImageView) findViewById(R.id.img);
 
-                final ImageView IMG;
-                IMG = (ImageView) findViewById(R.id.img);
+
+        Intent intent = getIntent();
+        String path = intent.getStringExtra("path");
+        File imageFile = new File(path);
+        Bitmap myBitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath()); //retrieving file
+
+        Matrix matrix = new Matrix();
+        matrix.postRotate(90);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(myBitmap, 0, 0, myBitmap.getWidth(), myBitmap.getHeight(), matrix, true);
+        //when placing a bitmap in image view from gallery, its rotated on its side :(
+        IMG.setImageBitmap(rotatedBitmap);
+        //IMG.setImageBitmap(myBitmap);
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+        myBitmap = Bitmap.createScaledBitmap(myBitmap, 160, 160, true);
+        myBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        final byte[] byteArray = stream.toByteArray();
+
+        final TextView tv = (TextView) findViewById(R.id.resultTv);
 
 
-                Intent intent = getIntent();
-                String path = intent.getStringExtra("path");
-                File imageFile = new File(path);
-                Bitmap myBitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath()); //retrieving file
-
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                myBitmap.compress(Bitmap.CompressFormat.PNG, 10, stream);
-                final byte[] byteArray = stream.toByteArray();
-
-                IMG.setImageBitmap(myBitmap);
-
+        //doing all this in a new thread
         new Thread(new Runnable() {
             public void run() {
 
-                onPictureTaken(byteArray);
+                DoorbellActivity.onPictureTaken(byteArray);
             }
         }).start();
     }
 
 
-
-
-
-    public Map<String, Float> annotateImage(byte[] imageBytes) throws IOException {
+    public static Map<String, Float> annotateImage(byte[] imageBytes) throws IOException {
         // Construct the Vision API instance
         HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
         JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
         VisionRequestInitializer initializer = new VisionRequestInitializer(CLOUD_VISION_API_KEY);
-        Vision vision = new Vision.Builder(httpTransport, jsonFactory, null)
-                .setVisionRequestInitializer(initializer).setApplicationName("vision")
-                .build();
+        Vision vision = new Vision.Builder(httpTransport, jsonFactory, null).setVisionRequestInitializer(initializer).setApplicationName("vision").build();
 
         // Create the image request
         AnnotateImageRequest imageRequest = new AnnotateImageRequest();
@@ -108,15 +116,13 @@ public class PassPhoto extends AppCompatActivity {
         // Batch and execute the request
         BatchAnnotateImagesRequest requestBatch = new BatchAnnotateImagesRequest();
         requestBatch.setRequests(Collections.singletonList(imageRequest));
-        BatchAnnotateImagesResponse response = vision.images()
-                .annotate(requestBatch)
-                .setDisableGZipContent(true)
-                .execute();
+        BatchAnnotateImagesResponse response = vision.images().annotate(requestBatch).setDisableGZipContent(true).execute();
 
         return convertResponseToMap(response);
         // More code here
-            }
-    private Map<String, Float> convertResponseToMap(BatchAnnotateImagesResponse response) {
+    }
+
+    private static Map<String, Float> convertResponseToMap(BatchAnnotateImagesResponse response) {
         Map<String, Float> annotations = new HashMap<>();
 
         // Convert response into a readable collection of annotations
@@ -129,15 +135,21 @@ public class PassPhoto extends AppCompatActivity {
 
         return annotations;
     }
-    private void onPictureTaken(final byte[] imageBytes) {
-        if (imageBytes != null) {
 
-            try {
-                // Process the image using Cloud Vision
-                Map<String, Float> annotations = annotateImage(imageBytes);
-                Log.d(TAG, "cloud vision annotations:" + annotations);
-            } catch (IOException e) {
-                Log.e(TAG, "Cloud Vison API error: ", e);
+    public static class DoorbellActivity extends Activity {
+
+        private static void onPictureTaken(final byte[] imageBytes) {
+            if (imageBytes != null) {
+
+                String a;
+
+                try {
+                    // Process the image using Cloud Vision
+                    Map<String, Float> annotations = annotateImage(imageBytes);
+                    Log.d(TAG, "cloud vision annotations:" + annotations);
+                } catch (IOException e) {
+                    Log.e(TAG, "Cloud Vison API error: ", e);
+                }
             }
         }
     }
